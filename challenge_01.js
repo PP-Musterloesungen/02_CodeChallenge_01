fd3f4c113729b812e82b957e3784c47fce918b5c
Robert <robelix@gmx.de>
2022-07-30 23:20:11 +0200

    meine Finale Version die läuft

--- challenge_01.js
+++ challenge_01.js
@@ -4,58 +4,79 @@
 /*
 Geben Sie über eine Prompt-Anforderung eine Zahl ein.
 Geben Sie über eine Prompt-Anforderung eine zweite Zahl ein.
-Berechnen Sie die Summe der beiden Zahlen.
+Berechnen Sie das Ergebnis der beiden Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
-"Die Summe der Zahlen ist: summe"
+"Die Summe der Zahlen ist: summe" => "Das Ergebnis lautet: "
 */
 
-// 
-/*********** Variante 2  ****************/
+/*  Meine Xte Lösung zur Programmieraufgabe ... */
+/* +, -, * und / samt Idiotenschleife! und es Funktioniert */
+/*********************************************************************************/
 
-// Kompaktere Lösung
+let zahl, zahl1, zahl2, operant, op;
 
-// let zahl1, zahl2, summe;
+// zahl1 = Number(prompt("Bitte Zahl 1 eingeben: "));
+zahl1 = getZahl();
 
-// zahl1 = parseInt(prompt("Zahl 1: "));
-// zahl2 = parseInt(prompt("Zahl 2: "));
-// summe = zahl1 + zahl2;
-// console.log("Die Summe der Zahlen ist: " + summe);
+// operant = prompt("Bitte Operanten eingeben ( + | - | * | / ): ");
+operant = getOperant();
 
-// /*********** Variante 3  ****************/
-// // Kompakt, aber schlechter lesbar, unflexibel
+zahl2 = Number(prompt("Bitte Zahl 2 eingeben: "));
+zahl2 = getZahl();
 
-// console.log
-// (
-//     "Die Summe ist: " + 
-//    (
-//     parseFloat(prompt("Bitte Zahl 1 eingeben")) +
-//     parseFloat(prompt("Bitte Zahl 2 eingeben"))
-//    ) 
-// );
+console.log("Das Ergebnis von " + zahl1 + " " + operant + " " + zahl2 + " = " + rechner(zahl1, operant, zahl2));
 
-/*  Meine Lösung zur Programmieraufgabe ... */
-
-let zahl1 = Number(prompt("Bitte Zahl 1 eingeben: "));
-let operant = prompt("Bitte Operanten eingeben ( + | - ): ");
-let zahl2 = Number(prompt("Bitte Zahl 2 eingeben: "));
-
-console.log("Das Ergebnis lautet: " + rechner(zahl1, operant, zahl2));
-
+function getZahl() {
+    do { zahl = Number(prompt("Bitte ein Zahl eingeben: "));  
+    } while (typeof zahl !== "number");
+    return zahl
+}
+/*********************************************************************************/
+function getOperant(){
+    do { operant = (prompt("Bitte Operanten eingeben ( + oder - oder * oder / ): "));
+       switch (operant) {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    op = true;
+                    break
+                default:
+                    op = false;
+                    console.log("Bitte einen korrekten Operanten eingeben!");
+                    break
+                };
+    } while (op == false) 
+    return operant
+};    
+/*********************************************************************************/
 function rechner(zahl1, operant, zahl2) {
     switch (operant) {
         case "+":
-            return console.log(summe(zahl1,zahl2));
+            return (summe(zahl1,zahl2));
         case "-":
-            return console.log(differenz(zahl1,zahl2));
+            return (differenz(zahl1,zahl2));
+        case "*":
+            return (multiplikation(zahl1,zahl2));
+        case "/":
+            return (division(zahl1,zahl2));
         default:
             return "Error!"
     }
 }
+/*********************************************************************************/
+function summe(zahl1, zahl2) { 
+    return (zahl1 + zahl2) }
 
-function summe(zahl1, zahl2) {
-    return (zahl1 + zahl2)
-}
+function differenz(zahl1, zahl2) { 
+    return (zahl1 - zahl2) }
 
-function differenz(zahl1, zahl2) {
-    return (zahl1 - zahl2)
-}
+function multiplikation(zahl1, zahl2) { 
+    return (zahl1 * zahl2) }
+
+function division(zahl1, zahl2) {
+      if (zahl2 != 0) { 
+        return (zahl1 / zahl2) }
+      else 
+        return console.log("Division durch 0 ist nicht erlaubt!")
+     }
